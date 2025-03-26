2881a372c4df369c4dfe9716284f5aa953d2dfcf
Macinas, Gabriel <gabriel.macinas@accenture.com>
2025-03-26 14:29:05 +0800

    fix: update .env loader

--- init.py
+++ init.py
@@ -8,10 +8,10 @@ import datetime;
 import subprocess;
 import os;
 from dotenv import load_dotenv
-load_dotenv()
 import signal
 import sys
 
+load_dotenv()
 CMD = '''
 on run argv
   display notification (item 2 of argv) with title (item 1 of argv)
